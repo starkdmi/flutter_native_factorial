2b88d996410e4ffd9a18fac8250ae65d3f98564a
Dmitry Starkov <21260939+starkdmi@users.noreply.github.com>
2023-06-06 13:46:45 +0300

    Format Swift code

--- darwin/Classes/Factorial/Factorial.swift
+++ darwin/Classes/Factorial/Factorial.swift
@@ -9,46 +9,46 @@ public class Factorial {
         queue: DispatchQueue = .global(),
         onProgress: ((Double) -> Void)? = nil,
         completion: @escaping (UInt64?) -> Void) throws -> FactorialTask {
-            guard number <= 20 else {
-                throw FactorialError.bigValue
-            }
-
-            // Cancellable object
-            let task = FactorialTask()
-
-            let max = Double(number) // all calls
-            var index = 0.0 // done calls
-
-            func factorial(_ num: UInt64) throws -> UInt64 {
-                // Cancellation
-                guard task.isCancelled == false else {
-                    throw FactorialError.cancelled
-                }
-
-                // Progress handling
-                if let callback = onProgress {
-                    index += 1.0
-                    callback(index / max)
-                }
-
-                // Slow down the calculation
-                Thread.sleep(forTimeInterval: 0.1) // usleep(100_000)
-
-                // Recursive implementation of factorial algorithm
-                return num <= 1 ? 1 : num * (try factorial(num - 1))
-            }
-
-            // Execute dispatched
-            queue.async {
-                do {
-                    let result = try factorial(UInt64(number))
-                    completion(result)
-                } catch {
-                    // cancelled
-                    completion(nil)
-                }
-            }
-
-            return task
+        guard number <= 20 else {
+            throw FactorialError.bigValue
         }
+
+        // Cancellable object
+        let task = FactorialTask()
+
+        let max = Double(number) // all calls
+        var index = 0.0 // done calls
+
+        func factorial(_ num: UInt64) throws -> UInt64 {
+            // Cancellation
+            guard task.isCancelled == false else {
+                throw FactorialError.cancelled
+            }
+
+            // Progress handling
+            if let callback = onProgress {
+                index += 1.0
+                callback(index / max)
+            }
+
+            // Slow down the calculation
+            Thread.sleep(forTimeInterval: 0.1) // usleep(100_000)
+
+            // Recursive implementation of factorial algorithm
+            return num <= 1 ? 1 : num * (try factorial(num - 1))
+        }
+
+        // Execute dispatched
+        queue.async {
+            do {
+                let result = try factorial(UInt64(number))
+                completion(result)
+            } catch {
+                // cancelled
+                completion(nil)
+            }
+        }
+
+        return task
+    }
 }
